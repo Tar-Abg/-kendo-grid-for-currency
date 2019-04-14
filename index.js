d5d91adfe5d4325ab1912f5512c962ba73145d24
Tar-Abg <46677613+Tar-Abg@users.noreply.github.com>
2019-04-14 15:37:17 +0400

    added kendo template

--- index.js
+++ index.js
@@ -1,19 +1,20 @@
 var $ = require ("jquery");
 import "@progress/kendo-ui/js/kendo.grid";
-require('./css/main.css')
+require('./css/style.css')
 
-export function createCurrency(rootElement, gridData,gridTitle){
+export function createCurrency(rootElement, outColumns, gridData, gridTitle){
     rootElement.append(
         $("<div>").append(
-            $("<div>").append( `<h3> class = "gridCurrency" ${gridTitle} </h3>`),
+            $("<div>").append( `<h3 class = "gridCurrencyTitle">  ${gridTitle} </h3>`),
             $("<div>").kendoGrid({
-                columns:titleArray,
+                columns:outColumns,
                 dataSource: {
                     data: gridData,
                 },
                 scrollable: false,
                 selectable: "cell"
-            })
-        )
-    )
+            }).addClass("kendoCurrencyGrid")
+        ).addClass("tableCurrency")
+    );
+    $("thead").hide()
 }
